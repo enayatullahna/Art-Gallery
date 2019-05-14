9dc19c9aae73153484d719b49fee80b4aff193f4
Enayatullah Naseri <ena.nas@icloud.com>
2019-05-14 11:04:49 -0700

    Application is now loading the image but after tapping like it will disapper

--- ArtGallery/PaintingListViewController.swift
+++ ArtGallery/PaintingListViewController.swift
@@ -8,23 +8,42 @@
 
 import UIKit
 
-class PaintingListViewController: UIViewController {
+class PaintingListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, PaintingTableViewCellDelegate {
+    func likeButtonTap(on cell: PaintingTableViewCell) {
+        guard let indexPath = tableView.indexPath(for: cell) else {return}
+        
+        let painting = paintingController.paintings[indexPath.row]
+        paintingController.toggleIsLiked(painting: painting)
+        tableView.reloadRows(at: [indexPath], with: .fade)
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
+        
+    }
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return paintingController.paintings.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PaintingCell", for: indexPath) as? PaintingTableViewCell else {return UITableViewCell() }
+        
+        let painting = paintingController.paintings[indexPath.row]
+        cell.delegate = self
+        cell.painting = painting
+        
+        return cell
     }
-    */
+    
+    let paintingController = PaintingController()
+    
 
 }
--- ArtGallery/PaintingTableViewCell.swift
+++ ArtGallery/PaintingTableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 
 protocol PaintingTableViewCellDelegate: class {
-    func likeButtonTap (on ell:
+    func likeButtonTap (on cell:
         PaintingTableViewCell)
 }
 class PaintingTableViewCell: UITableViewCell {
